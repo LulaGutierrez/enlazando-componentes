726e4a019d7c5bab09a0a7cffd950008b8918379
LulaGutierrez <lujanvicgutierrez@gmail.com>
2023-01-15 17:44:00 -0300

    uno mas

---
+++ dashboard/src/components/ContentWrapper.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import ContentRowTop from './ContentRowTop';
+
+function ContentWrapper(){
+    return (
+        <React.Fragment>
+            <ContentRowTop />
+        </React.Fragment>
+    )
+}
+export default ContentWrapper;
